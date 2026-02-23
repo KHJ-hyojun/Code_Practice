fc04e318454890159071286982396a3390c21fc8
KHJ-hyojun <kgywns03@gmail.com>
2026-02-23 12:44:33 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2264 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> dp(n + 1, 1e9);
+	dp[0] = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j * j <= i; j++)
+		{
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+	cout << dp[n];
+}
