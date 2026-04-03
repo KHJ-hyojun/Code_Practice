ee07d799f6fd86b856e89c861c998a7db44e3f94
KHJ-hyojun <kgywns03@gmail.com>
2026-04-03 15:07:35 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int dp[1001][3] = {0};
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> dp[i][j];
+		}
+	}
+
+	for (int i = 1; i < n; i++)
+	{
+		dp[i][0] = min(dp[i - 1][1] + dp[i][0], dp[i - 1][2] + dp[i][0]);
+		dp[i][1] = min(dp[i - 1][0] + dp[i][1], dp[i - 1][2] + dp[i][1]);
+		dp[i][2] = min(dp[i - 1][0] + dp[i][2], dp[i - 1][1] + dp[i][2]);
+	}
+
+	cout << min({ dp[n - 1][0], dp[n - 1][1], dp[n - 1][2] });
+
+}
